03be812b3db86aaa4b83af94755b136e1e7aedc2
Art Hundiak <ahundiak@gmail.com>
2013-09-02 21:31:13 -0500

    Added projects repository.

--- src/Cerad/Bundle/PersonBundle/Form/Type/USSF/OrgStateFormType.php
+++ src/Cerad/Bundle/PersonBundle/Form/Type/USSF/OrgStateFormType.php
@@ -10,26 +10,21 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  * Use this to collect and partially validate a region number
  * The transformer will yield AYSORxxxx
  */
-class LeagueFormType extends AbstractType
-{   
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-    }
+class OrgStateFormType extends AbstractType
+{
+    public function getParent() { return 'choice'; }
+    public function getName()   { return 'cerad_person_ussf_org_state'; }
+
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-          //'invalid_message' => 'Unknown Region Number',
-            
             'label'    => 'State Certified In',
-            'choices'  => $this->leagueChoices,
+            'choices'  => $this->choices,
             'multiple' => false,
             'expanded' => false,
         ));
     }
-    public function getParent() { return 'choice'; }
-    public function getName()   { return 'cerad_person_ussf_leagues'; }
-    
-    protected $leagueChoices = array
+    protected $choices = array
     (
         'USSF_AL' => 'Alabama',
         'USSF_AR' => 'Arkansas',
